45c9730da0d90e97e15bb451f75e84d402f558bf
JoyOffiong <offiongjoyasuquo@gmail.com>
2022-06-15 04:53:23 +0100

    improvement of basic styles and removal of overflow

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const menuButton=document.querySelector('.nav-toggle');
 const navigationMenu=document.querySelector('.mobile-links');
 const closeButton=document.querySelector('.nav-toggle2');
+const mainWrap = document.querySelector('.main-wrap')
 
 
 menuButton.addEventListener('click', () => {
@@ -8,6 +9,7 @@ menuButton.addEventListener('click', () => {
         menuButton.style.display='none'
         closeButton.style.display = 'block'
         navigationMenu.style.transform = 'translateX(0%)'
+        mainWrap.style.overflow= 'hidden'
 })
  
     closeButton.addEventListener('click', () => {
